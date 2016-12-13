59d1f9b3d5c45ea3a9c6eb52973b317e66fce01b
Christian Skar <skarchr@gmail.com>
2016-12-13 01:46:37 +0100

    Moved development dependent files inside their gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var exec = require('child_process').exec;
-var watch = require('gulp-watch');
-var gutil = require('gulp-util');
 
 gulp.task('browserify', function () {
     return browserify('./public/scripts/app.js')
@@ -21,6 +18,9 @@ gulp.task('default', [
 
 gulp.task('watch', function () {
 
+    var watch = require('gulp-watch');
+    var gutil = require('gulp-util');
+
     var watcher = gulp.watch('./public/scripts/**/*.js', ['browserify']);
     watcher.on('change', function (event) {
         gutil.log('Script changes detected...');
@@ -29,6 +29,8 @@ gulp.task('watch', function () {
 });
 
 gulp.task('serve', ['browserify', 'watch'], function () {
+    var exec = require('child_process').exec;
+
     exec('node server.js', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
